4b24b385e68d069efd3cd30693acd0ad5bf40c2f
nodahiroto <nh419@icloud.com>
2022-03-08 23:08:46 +0900

    Adding a Class of Service for User Registration

--- src/main/java/com/example/demo/service/AppUserService.java
+++ src/main/java/com/example/demo/service/AppUserService.java
@@ -1,5 +1,31 @@
 package com.example.demo.service;
 
-public class AppUserService {
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import com.example.demo.model.AppUser;
+import com.example.demo.repository.AppUserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class AppUserService {
+	
+	private final AppUserRepository userRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
+	
+	public boolean RegistrationUser(AppUser appUser) {
+		
+		if(userRepository.existsByEmail(appUser.getEmail()) == true) {
+			return false;
+		}
+		
+//		if(passwordEncoder) {
+//			
+//		}
+		userRepository.save(appUser);
+		return true;
+		
+	}
 }
