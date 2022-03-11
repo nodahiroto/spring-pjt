d8b9ef3b5fc426556cfe64f2c98fffc5622a32d5
nodahiroto <nh419@icloud.com>
2022-03-11 22:28:42 +0900

    Add reset method

--- src/main/java/com/example/demo/controller/ManeyManagementController.java
+++ src/main/java/com/example/demo/controller/ManeyManagementController.java
@@ -1,5 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class ManeyManagementController {
 
+	@PostMapping("/reset")
+	public void reset() {
+		// 予算のリセット処理
+	}
 }
