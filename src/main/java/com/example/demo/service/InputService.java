01dc75778bacca5873bb8bc090606b51367ef461
nodahiroto <nh419@icloud.com>
2022-03-12 10:49:50 +0900

    Add payment method

--- src/main/java/com/example/demo/service/InputService.java
+++ src/main/java/com/example/demo/service/InputService.java
@@ -1,5 +1,26 @@
 package com.example.demo.service;
 
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Input;
+import com.example.demo.repository.InputRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
 public class InputService {
 
+	private final InputRepository inputRepository;
+	
+	public Input payment(Input input) {
+		// 予算に反映
+		
+		
+		input.setCreatedAt(LocalDateTime.now());
+		inputRepository.save(input);
+		return input;
+	}
 }
