e5e3672e1e9aea2a775b7cc9cfe6acd5536d7a42
nodahiroto <nh419@icloud.com>
2022-03-14 22:59:09 +0900

    Addition of editing function

--- src/main/java/com/example/demo/controller/ManeyManagementController.java
+++ src/main/java/com/example/demo/controller/ManeyManagementController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -31,11 +34,15 @@ public class ManeyManagementController {
 	
 	@GetMapping("/home")
 	public String home(Model model) {
+		// 入金の合計
 		int allInput = inputRepository.getAllInput();
+		// 支出の合計
 		int allOutput = outputRepository.getAllOutput();
 		
+		// 現在の予算
 		Budget budget = budgetService.countBudget(allInput, allOutput);
 		model.addAttribute("budgetRemain", budget.getNowBudget());
+		
 		return "/home";
 	}
 	
@@ -47,6 +54,7 @@ public class ManeyManagementController {
 	
 	@GetMapping("/input")
 	public String inputForm(@ModelAttribute Input input) {
+		
 		return "/input";
 	}
 	
@@ -67,6 +75,7 @@ public class ManeyManagementController {
 	
 	@GetMapping("/output")
 	public String outputForm(@ModelAttribute Output output) {
+		
 		return "/output";
 	}
 	
@@ -82,6 +91,52 @@ public class ManeyManagementController {
 		outputService.expenditure(output);
 		redirectAttributes.addFlashAttribute("message", "支出しました。");
 
+		return "/home";
+	}
+	
+	@GetMapping("/detail")
+	public String detailList(Model model) {
+		// 入金履歴の取得
+		List<Input> inputList = inputRepository.findAll();
+		// 支出履歴の取得
+		List<Output> outputList = outputRepository.findAll();
+		
+		// 入金の合計を取得
+		int allInput = inputRepository.getAllInput();
+		// 支出の合計を取得
+		int allOutput = outputRepository.getAllOutput();
+		
+		model.addAttribute("inputList", inputList);
+		model.addAttribute("outputList", outputList);
+		model.addAttribute("allInput", allInput);
+		model.addAttribute("allOutput", allOutput);
+		
+		return "/detail";
+	}
+	
+	@GetMapping("/input/edit/{id}")
+	public String inputEdit(@PathVariable Long id, Model model) {
+		Input input = inputRepository.findById(id).get();
+		model.addAttribute("input", input);
+		
+		return "/input-edit";
+	}
+	
+	@PostMapping("/input/update/{id}")
+	public String inputUpdate(@PathVariable Long id, @ModelAttribute Input input,
+			RedirectAttributes redirectAttributes) {
+		
+		inputRepository.save(input);
+		redirectAttributes.addFlashAttribute("message", "データを更新しました。");
+		
+		return "/home";
+	}
+	
+	@GetMapping("/input/delete/{id}")
+	public String inputDelete(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+		inputRepository.deleteById(id);
+		redirectAttributes.addFlashAttribute("message", "データを削除しました。");
+		
 		return "/home";
 	}
 }
