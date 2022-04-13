9928d11949ce06af7a347654aab4e5e5f25567b4
nodahiroto <nh419@icloud.com>
2022-04-13 22:31:37 +0900

    add bean

---
+++ src/main/java/com/example/demo/model/beans/TotalMonth.java
@@ -0,0 +1,33 @@
+package com.example.demo.model.beans;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class TotalMonth implements Serializable {
+
+	private int january;
+	
+	private int february;
+	
+	private int march;
+	
+	private int april;
+	
+	private int may;
+	
+	private int june;
+	
+	private int july;
+	
+	private int august;
+	
+	private int september;
+	
+	private int october;
+	
+	private int november;
+	
+	private int december;
+}
